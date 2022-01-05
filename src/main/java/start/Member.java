b1bd70f0bf832455f4735106c067b4c443f923fb
HYEBIN,PARK <hyeqls3742@gmail.com>
2022-01-05 21:13:39 +0900

    제약 조건 추가

--- src/main/java/start/Member.java
+++ src/main/java/start/Member.java
@@ -1,22 +1,39 @@
 package start;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@Table(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"})}) // 유니크 제약조건 추가
 public class Member {
 
     @Id
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10) // 제약조건 추가, 회원 이름은 필수로 입력되어야하고 10자를 초과하면 안 된다.
     private String username;
 
-//    private Integer age;
+    private Integer age;
+
+
+    // enum을 사용하려면 @Enumerated 으로 매핑해야함
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    // @Temporal 을 사용해서 날짜 타입은 매핑해야함
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    // CLOB, BLOB 매핑 가능
+    @Lob
+    private String description;
+
 
     public String getId(){
         return id;
@@ -34,12 +51,12 @@ public class Member {
         this.username = username;
     }
 
-//    public Integer getAge(){
-//        return age;
-//    }
-//
-//    public void setAge(Integer age){
-//        this.age = age;
-//    }
+    public Integer getAge(){
+        return age;
+    }
+
+    public void setAge(Integer age){
+        this.age = age;
+    }
 
 }
